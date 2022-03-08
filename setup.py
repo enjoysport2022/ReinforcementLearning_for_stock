86ec457536f17aa5ea25fccc3df1ec72ac3dea98
poteman <946691288@qq.com>
2022-03-08 16:47:50 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name="ReinforcementLearning_for_stock",
+        version="0.0.1",
+        description="rl for stock",
+        url='https://github.com/poteman/ReinforcementLearning_for_stock',
+        install_requires=[
+            'lightgbm',
+            'xgboost',
+            'torch',
+            'numpy',
+            'pandas',
+            'sklearn',
+            'tqdm'
+        ],
+        python_requires='>=3.6',
+        packages=find_packages(exclude=['data', 'img', 'log']),
+        include_package_data=True,
+        zip_safe=False
+        )
+# python setup.py sdist build
+# twine upload dist/*
